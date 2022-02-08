d86550e1d3d67066fc1b38943eec9af105f6c3fe
Funjabi1 <karansingh.26898@gmail.com>
2022-02-08 19:43:50 +0530

    fished tempreture api call

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import Forecast from './components/Forecast';
 
 function App() {
+
+  const api_key = process.env.REACT_APP_API_KEY;
+  const api_url = `https://api.openweathermap.org/data/2.5/`;
+  
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState([]); 
+  
+     useEffect(() => {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+    //    
+      const getData = () => {
+        axios.get(`http://${api_url}weather?lat=${lat}&lon=${long}&units=metric&appid=${api_key}`)
+        .then(res => {
+          setData(res.data);  
+      })}
+      getData();
+      console.log("latitude is:", lat)
+      console.log("longitude is:",long)
+      },[lat, long]);
+
+  const search = (event) => {
+     axios.get(`http://${api_url}weather?q=${query}&units=metric&appid=${api_key}`)
+    .then(res => {
+      setData(res.data);
+      console.log(res.data);
+    }
+  )
+  }
+
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div>
+          {data.main && (
+            <div className={data.main.temp > 20?'weather-box':'weather-box-cold'}>
+              <div className="search-box">
+                <input type="text" placeholder="search..."
+                onChange={e =>setQuery(e.target.value)}
+                value={query}
+                onKeyPress={(e) => {if(e.key === 'Enter') search()}} />
+              <button onClick={search}>search</button>
+            </div>
+                  <div className=''>{data.name}, {data.sys.country}</div>            
+                  <div> Temp:<div>{data.main.temp}°C</div></div>
+                  <div> Humidity: {data.main.humidity}%</div>
+                  <div> Pressure:{data.main.pressure} kPa </div>
+                  <div> Wind:{data.wind.speed} km/h </div> 
+                  <div> Description:{data.weather[0].description} </div>   
+                  <div> Sunrise:{new Date(data.sys.sunrise * 1000).toLocaleTimeString()} </div>
+            </div>)}
+        </div>
+        <Forecast lat={lat} long={long}/>
+      </main>
     </div>
   );
-}
+ }
 
 export default App;
+
+
